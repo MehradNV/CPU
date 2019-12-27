57822ef577784438bab144173293da3b2cf94212
MehradNV <49492726+MehradNV@users.noreply.github.com>
2019-12-28 00:15:01 +0330

    ALU finished

--- ALU.v
+++ ALU.v
@@ -1,4 +1,27 @@
-module ALU(read_data_1,read_data_2,ALUcontrol,ALUresult);
+module ALU(clk,read_data_1,read_data_2,ALUcontrol,ALUresult);
   
-  input read_data_1[4:0],read_data_2[4:0],ALUcontrol[4:0];
-  output ALUresult
+  input clk;
+  input [4:0] read_data_1,read_data_2;
+  wire [4:0] r1,r2;
+  input [3:0] ALUcontrol;
+  output [4:0] ALUresult;
+  reg [4:0] result;
+  
+  assign r1 = read_data_1;
+  assign r2 = read_data_2;
+  assign ALUresult = result;
+  
+  always@(posedge clk)
+    begin
+      case(ALUcontrol)
+      
+      0: result = r1 & r2;
+         
+      1: result = r1 | r2;
+        
+      2: result = r1 + r2;
+        
+      6: result = r1 - r2;
+      endcase  
+    end
+endmodule
